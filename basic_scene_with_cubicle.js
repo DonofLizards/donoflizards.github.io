0f3cebf42cbbe323bd2b5f040c419c87440caeda
DonofLizards <katie@Katie.local>
2014-07-16 18:10:40 +0100

    Fixed syntax, added intersections code

--- basic_scene_with_cubicle.js
+++ basic_scene_with_cubicle.js
@@ -2,104 +2,72 @@
 $(document).ready(function() {
 
 var stats = initStats();
-
 var controls;
 cubicleMeshes = [];
 var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
 var instructions = document.getElementById('instructions');
 
-if (havePointerLock)
-{
+if (havePointerLock) {
 	var element = document.body;
 	
 	var pointerLockChange = function(evt) {
 	
-		if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element)
-			{
+		if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
 				controls.enabled = true;
 				container.style.display = 'none';
-				}
-		else 
-			{	
-				controls.enabled = false;
+			}
+		else {	
+			controls.enabled = false;
+			}
 
-				}
-
-		};
 	};
+};
 	
-	document.addEventListener( 'pointerlockchange', pointerLockChange, false );
-	document.addEventListener( 'mozpointerlockchange', pointerLockChange, false );
-	document.addEventListener( 'webkitpointerlockchange', pointerLockChange, false );
+document.addEventListener( 'pointerlockchange', pointerLockChange, false );
+document.addEventListener( 'mozpointerlockchange', pointerLockChange, false );
+document.addEventListener( 'webkitpointerlockchange', pointerLockChange, false );
 	
-	instructions.addEventListener('click', function(evt) {
-		instructions.style.display = 'none';
+instructions.addEventListener('click', function(evt) {
+	instructions.style.display = 'none';
 
-		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+	element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
 
 	if ( /Firefox/i.test( navigator.userAgent ) ) {
 
 		var fullscreenchange = function ( event ) {
 
-		if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+			if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
 
-			document.removeEventListener( 'fullscreenchange', fullscreenchange );
-			document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+				document.removeEventListener( 'fullscreenchange', fullscreenchange );
+				document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
 
-			element.requestPointerLock();
+				element.requestPointerLock();
+
 			}
 
+		}	
+
+		document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+		document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+		element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+		element.requestFullscreen();
+
 		}
-		
 
 
-	document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-	document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-
-	element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
-
-	element.requestFullscreen();
-	}
-
-
-	else 
-	{
+	else {
 		element.requestPointerLock();
 		}
 		
-	});
+});
 		
-/*// PointerLock requested when element is clicked.
-//function getPointerLock() {
-	
-	
-	var instructions = document.getElementById("instructions");
-	
-	instructions.addEventListener('click', function(evt) {
-		instructions.style.display = 'none';
-		
-		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-		
-		//document.addEventListener('keypress', moveCamera, false);
-
-		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock; 
-		
-		// Ask browser to lock the pointer.
-		element.requestPointerLock();
-		
-		});
-//}
-
-*/
+// Load Blender model 
 var loader = new THREE.JSONLoader();
 loader.load("cubicle_generic_GIANT.js", addModelToScene);
 
-function addModelToScene( geometry, materials ) 
-{
-	
-	
+function addModelToScene( geometry, materials ) {
+
 	for (var x = 0; x < 10; x ++) 
 	{
 		var material = new THREE.MeshFaceMaterial( materials );
@@ -116,23 +84,25 @@ function addModelToScene( geometry, materials )
  		cubicleMeshes.push(cubicleMesh);
 		
 	}
+
 }
 
 function randomSign(int) {
 
-	if (Math.round(Math.random()) === 1)
-	{
+	if (Math.round(Math.random()) === 1) {
 		return -int;
 	}
 	else {
 		return int;
 	}
+
 }
 
 init();
 renderScene();
 
 function initStats() {
+
                var stats = new Stats();
                stats.setMode(0);
                stats.domElement.style.position = 'absolute';
@@ -140,6 +110,7 @@ function initStats() {
                stats.domElement.style.top = '0px';
                $("#statsOutput").append( stats.domElement );
                return stats;
+
 }
 	
 function init () {	
@@ -156,7 +127,8 @@ function init () {
   	hemiLight.position.set(0, 500, 0);
    	scene.add(hemiLight);
 
-   	    var lightControls = new function () {
+   	var lightControls = new function () {
+
             	this.rotationSpeed = 0.03;
             	this.bouncingSpeed = 0.03;
 
@@ -171,19 +143,16 @@ function init () {
 
         	gui.add(lightControls, 'hemisphere').onChange(function (e) {
             	hemiLight.visible = e;
-        });
+        	});
         	gui.addColor(lightControls, 'groundColor').onChange(function (e) {
             	hemiLight.groundColor = new THREE.Color(e);
-        });
+       	});
        	 gui.addColor(lightControls, 'skyColor').onChange(function (e) {
             	hemiLight.color = new THREE.Color(e);
-        });
+       	});
         	gui.add(lightControls, 'intensity', 0, 5).onChange(function (e) {
            	 hemiLight.intensity = e;
-        });
-
-
-   	
+        	});
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setClearColor(0xEEEEEE);
@@ -214,7 +183,8 @@ function init () {
 	camera.position.z = 400;
 
 	document.body.appendChild(renderer.domElement);
-	}
+
+}
 
 
 
@@ -244,15 +214,28 @@ function init () {
 	
 function renderScene() {
 	stats.update();
+
 	requestAnimationFrame(renderScene);
+
 	controls.isOnObject(false);
+
 	ray.ray.origin.copy( controls.getObject().position );
 	ray.ray.origin.y -= 10;
+
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-	//getPointerLock();
+
 	controls.update();
+
 	renderer.render(scene, camera);
+
+	var intersections = raycaster.intersectObjects( objects );
+
+	if ( intersections.length > 0 ) {
+		//controls.isOnObject( true );
+		alert("true");
+	}
+
 	/*for (var i = 0; i <  monkeyModels.length ; i ++)
 	 {
 		monkeyModels[i].rotation.x += randomSign(Math.random() / 10);
@@ -278,7 +261,7 @@ function renderScene() {
 
 	}*/
 
-	}
+}
 	
 });
 
